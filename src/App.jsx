fda797100c88681f4876612b28d1fe82da6d7dd9
amilcarjunioraj-dev <amilcar.junior.aj@gmail.com>
2026-06-02 03:01:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import Tela1Home from './screens/Tela1Home';
 import Tela2Home from './screens/Tela2Home';
 import Tela2Descricao from './screens/Tela2Descricao';
-import Tela2Upload from './screens/Tela2Upload';
 import Tela3GerarReferencias from './screens/Tela3GerarReferencias';
 import Tela4Tamanho from './screens/Tela4Tamanho';
 import Tela5Cor from './screens/Tela5Cor';
@@ -15,10 +14,8 @@ import Tela9Confirmacao from './screens/Tela9Confirmacao';
 function App() {
   const [tela, setTela] = useState(1);
   const [dados, setDados] = useState({
-    modo: '', // 'upload' ou 'descrever'
+    modo: '',
     descricao_cliente: '',
-    referencia_escolhida: 0,
-    referencia_descricao: '',
     tamanho: 8,
     cor: '',
     cor_nome: '',
@@ -26,18 +23,10 @@ function App() {
     local: '',
     local_nome: '',
     preco: 0,
-    forma_pagamento: 'pix',
-    nome: '',
-    whatsapp: '',
-    email: '',
-    dia: '',
-    hora: '',
   });
 
   const atualizarDados = (novosDados) => {
-    const dadosAtualizados = { ...dados, ...novosDados };
-    setDados(dadosAtualizados);
-    localStorage.setItem('orcamento_bill', JSON.stringify(dadosAtualizados));
+    setDados({ ...dados, ...novosDados });
   };
 
   const irParaTela = (numeroTela) => {
@@ -45,32 +34,29 @@ function App() {
     window.scrollTo(0, 0);
   };
 
+  console.log('Tela atual:', tela, 'Modo:', dados.modo);
+
   return (
     <div className="app">
-      {/* TELA 1: HOME */}
       {tela === 1 && <Tela1Home irParaTela={irParaTela} />}
       
-      {/* TELA 2: ESCOLHER UPLOAD OU DESCREVER */}
       {tela === 2 && <Tela2Home irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
       
-      {/* ===== FLUXO UPLOAD ===== */}
-      {tela === 3 && dados.modo === 'upload' && <Tela2Upload irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 4 && dados.modo === 'upload' && <Tela4Tamanho irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 5 && dados.modo === 'upload' && <Tela5Cor irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 6 && dados.modo === 'upload' && <Tela6Local irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 7 && dados.modo === 'upload' && <Tela7Resultado irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 8 && dados.modo === 'upload' && <Tela7Agendamento irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 9 && dados.modo === 'upload' && <Tela9Confirmacao dados={dados} irParaTela={irParaTela} />}
-      
-      {/* ===== FLUXO DESCREVER ===== */}
       {tela === 3 && dados.modo === 'descrever' && <Tela2Descricao irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      
       {tela === 4 && dados.modo === 'descrever' && <Tela3GerarReferencias irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 5 && dados.modo === 'descrever' && <Tela4Tamanho irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 6 && dados.modo === 'descrever' && <Tela5Cor irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 7 && dados.modo === 'descrever' && <Tela6Local irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 8 && dados.modo === 'descrever' && <Tela7Resultado irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 9 && dados.modo === 'descrever' && <Tela7Agendamento irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 10 && dados.modo === 'descrever' && <Tela9Confirmacao dados={dados} irParaTela={irParaTela} />}
+      
+      {tela === 5 && <Tela4Tamanho irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      
+      {tela === 6 && <Tela5Cor irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      
+      {tela === 7 && <Tela6Local irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      
+      {tela === 8 && <Tela7Resultado irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      
+      {tela === 9 && <Tela7Agendamento irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      
+      {tela === 10 && <Tela9Confirmacao dados={dados} irParaTela={irParaTela} />}
     </div>
   );
 }
