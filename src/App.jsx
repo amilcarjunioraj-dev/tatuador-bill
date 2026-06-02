7edcf04b94e7656b42cbde3a9695efb699e87b71
amilcarjunioraj-dev <amilcar.junior.aj@gmail.com>
2026-06-01 23:49:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-import Tela1_Home from './screens/Tela1_Home';
-import Tela2_Estilo from './screens/Tela2_Estilo';
-import Tela3_Tamanho from './screens/Tela3_Tamanho';
-import Tela4_Cor from './screens/Tela4_Cor';
-import Tela5_Local from './screens/Tela5_Local';
-import Tela6_Resultado from './screens/Tela6_Resultado';
-import Tela7_Agendamento from './screens/Tela7_Agendamento';
-import Tela8_Confirmacao from './screens/Tela8_Confirmacao';
+import Tela1Home from './screens/Tela1Home';
+import Tela2Estilo from './screens/Tela2Estilo';
+import Tela3Tamanho from './screens/Tela3Tamanho';
+import Tela4Cor from './screens/Tela4Cor';
+import Tela5Local from './screens/Tela5Local';
+import Tela6Resultado from './screens/Tela6Resultado';
+import Tela7Agendamento from './screens/Tela7Agendamento';
+import Tela8Confirmacao from './screens/Tela8Confirmacao';
 
 function App() {
   const [tela, setTela] = useState(1);
@@ -39,14 +39,14 @@ function App() {
 
   return (
     <div className="app">
-      {tela === 1 && <Tela1_Home irParaTela={irParaTela} />}
-      {tela === 2 && <Tela2_Estilo irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 3 && <Tela3_Tamanho irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 4 && <Tela4_Cor irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 5 && <Tela5_Local irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 6 && <Tela6_Resultado irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 7 && <Tela7_Agendamento irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
-      {tela === 8 && <Tela8_Confirmacao dados={dados} irParaTela={irParaTela} />}
+      {tela === 1 && <Tela1Home irParaTela={irParaTela} />}
+      {tela === 2 && <Tela2Estilo irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      {tela === 3 && <Tela3Tamanho irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      {tela === 4 && <Tela4Cor irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      {tela === 5 && <Tela5Local irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      {tela === 6 && <Tela6Resultado irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      {tela === 7 && <Tela7Agendamento irParaTela={irParaTela} dados={dados} atualizarDados={atualizarDados} />}
+      {tela === 8 && <Tela8Confirmacao dados={dados} irParaTela={irParaTela} />}
     </div>
   );
 }
